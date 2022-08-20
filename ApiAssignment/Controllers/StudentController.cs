88aa83192be5447dbd49fe2bfe7dc20aa79f6a7d
fady george <34046688+fadygeorge@users.noreply.github.com>
2022-08-20 13:45:52 +0200

    Add files via upload

---
+++ ApiAssignment/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using IdentityAuthhApi.Data;
+using IdentityAuthhApi.Logic;
+using IdentityAuthhApi.Logic.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IdentityAuthApi.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    
+    public class StudentController : ControllerBase
+    {
+        UnitOfWork unitofwork;
+      
+        public StudentController(Context context)
+        {
+            unitofwork = new UnitOfWork(context);
+        }
+        
+        public IEnumerable<StudentViewModel> Get()
+        {
+            var model = unitofwork.Student.Get();
+            return model;
+        }
+
+        public ResultViewModel Add(StudentViewModel s)
+        {
+            unitofwork.Student.Add(s);
+            unitofwork.Commit();
+            return new ResultViewModel { IsSuccess = true, };
+        }
+
+        public StudentViewModel GetS(int id)
+        {
+            return unitofwork.Student.Get(id);
+        }
+
+        //[Authorize]
+        public ResultViewModel Edit(StudentViewModel s)
+        {
+            unitofwork.Student.Edit(s);
+            unitofwork.Commit();
+            return new ResultViewModel { IsSuccess = true, };
+        }
+
+        public ResultViewModel Delete(int id)
+        {
+            unitofwork.Student.Delete(id);
+            unitofwork.Commit();
+            return new ResultViewModel { IsSuccess = true, };
+        }
+        
+    }
+
+
+}
