b6df7ca1e6dae1816944cc2bd3270962f84316c2
fady george <34046688+fadygeorge@users.noreply.github.com>
2022-08-20 14:34:52 +0200

    added ApiAssignmetn.Web

---
+++ ApiAssignment.Web/Controllers/StudentController.cs
@@ -0,0 +1,147 @@
+﻿using IdentityAuthhApi.Logic.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace IdentityAuthApi.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            RestClient client = new RestClient("https://localhost:44353/api/Student/Get"); // localhost
+            RestRequest request = new RestRequest();
+            string s = "Bearer " + GetToken(); // token from postman or function
+            request.AddHeader("Authorization", s);
+            var response = client.ExecuteGet(request);
+            var model = JsonConvert.DeserializeObject<IEnumerable<StudentViewModel>>(response.Content);
+            return View(model);
+        }
+
+        public string GetToken()
+        {
+            RestClient client = new RestClient("https://localhost:44353/api/Account/Login"); // localhost
+            RestRequest request = new RestRequest();
+            var model = new LoginViewModel { Username = "Army", Password = "Aa@123456" };
+            request.AddBody(model);
+;           // s = "Bearer *****"; // token from postman
+            //request.AddHeader("Authorization", s);
+            var response = client.ExecutePost(request);
+            var res = JsonConvert.DeserializeObject<ResultViewModel>(response.Content);
+            return res.Data.ToString();
+        }
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            RestClient client = new RestClient("https://localhost:44353/api/Student/GetS"); // localhost
+            RestRequest request = new RestRequest(); // Config API
+            request.AddParameter("id", id); // Send parameters to .Api as json
+            var response = client.ExecuteGet(request);
+            var model = JsonConvert.DeserializeObject<StudentViewModel>(response.Content); // Consume response returned from API
+            return View(model);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(StudentViewModel model)
+        {
+            try
+            {
+                RestClient client = new RestClient("https://localhost:44353/api/Student/Add"); // localhost
+                RestRequest request = new RestRequest();
+                request.AddBody(model);
+                var response = client.ExecuteGet(request);
+                var result = JsonConvert.DeserializeObject<ResultViewModel>(response.Content);
+                if (result.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(model);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            RestClient client = new RestClient("https://localhost:44353/api/Student/GetS"); // localhost
+            RestRequest request = new RestRequest(); // Config API
+            request.AddParameter("id", id);
+            var response = client.ExecuteGet(request);
+            var model = JsonConvert.DeserializeObject<StudentViewModel>(response.Content); // Consume response returned from API
+            return View(model);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, StudentViewModel model)
+        {
+            try
+            {
+                RestClient client = new RestClient("https://localhost:44353/api/Student/Edit"); // localhost
+                RestRequest request = new RestRequest();
+                request.AddBody(model);
+                var response = client.ExecuteGet(request);
+                var result = JsonConvert.DeserializeObject<ResultViewModel>(response.Content);
+                if (result.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(model);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            RestClient client = new RestClient("https://localhost:44353/api/Student/GetS"); // localhost
+            RestRequest request = new RestRequest(); // Config API
+            request.AddParameter("id", id);
+            var response = client.ExecuteGet(request);
+            var model = JsonConvert.DeserializeObject<StudentViewModel>(response.Content); // Consume response returned from API
+            return View(model);
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, StudentViewModel model)
+        {
+            try
+            {
+                RestClient client = new RestClient("https://localhost:44353/api/Student/Delete"); // localhost
+                RestRequest request = new RestRequest(); // Config API
+                request.AddParameter("id", id);
+                var response = client.ExecuteGet(request);
+                var res = JsonConvert.DeserializeObject<ResultViewModel>(response.Content); // Consume response returned from API
+                if (res.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(model);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
